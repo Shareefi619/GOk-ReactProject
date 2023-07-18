6d59981bd5705ab3db845276e7536a28a238aef4
Shareefi619 <irfanhaidershareefi6@gmail.com>
2023-07-18 18:55:43 +0500

    4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,28 @@ import axios, { AxiosResponse } from "axios";
 import Card from "./Card/Card";
 import { CardProps } from "./Card/Card";
 
-// interface Card {
-//   id: { value: string };
-//   name: { first: string; last: string; title: string };
-//   gender: string;
-//   email: string;
-// }
-
 const Page: React.FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [cards, setCards] = useState<CardProps[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response: AxiosResponse<{ results: CardProps[] }> = await axios.get(
-          "https://randomuser.me/api/?results=50"
-        );
-
-        const responseNew = response.data.results.filter((fit) => fit.id.value);
-        const filteredValues = responseNew.filter((value, index) => index < 20);
-        //console.log(filteredValues);
+        const response: AxiosResponse<{ results: CardProps[] }> =
+          await axios.get("https://randomuser.me/api/?results=50");
+        const filteredValues = response.data.results
+          .filter((fit) => fit.id.value)
+          .slice(0, 20);
+        console.log(filteredValues);
         setCards(filteredValues);
-        //setCards(response.data.results);
-        //console.log(responseNew);
+        if (!response.status) {
+          throw new Error("Data Not Found!");
+        }
         setIsLoading(false);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        const newError = error;
+        console.log(newError);
+      } finally {
         setIsLoading(false);
       }
     };
@@ -39,37 +34,37 @@ const Page: React.FC = () => {
 
   return (
     <div>
-      <header className="bg-gray-200 p-4">
-        {/* Header content */}
-        <h1 className="text-xl font-bold">My Page Header</h1>
+      <header className="flex justify-center bg-gray-800 text-white p-4">
+        <h1 className="text-2xl font-bold">My React App!</h1>
       </header>
 
-      <main className="p-4">
+      <main>
+        /** Loader: code */
         {isLoading ? (
           <div className="flex justify-center items-center h-full">
             <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
           </div>
-        ) : (
-          <div >
+          
+        ) : cards.length > 0 ? (
+          <div>
             {cards.map((card) => (
-              
-              // <div 
-              //   key={card.id.value}
-              //   className="bg-gray-200 rounded-lg shadow-md p-6 border border-4 border-indigo-200 border-y-indigo-500"
-              // >
-              //   {/* Card content */}
-              //   <h2 className="text-2xl font-bold mb-4">{card.name.title} {card.name.first} {card.name.last}</h2>
-              //   <p className="text-black-200">Gender: {card.gender}</p>
-              //   <p className="text-black-200">Email: {card.email}</p>
-              // </div>
-              <Card  id = {card.id} name={card.name} gender={card.gender} email={card.gender}/>
-            
+              <Card
+                id={card.id}
+                name={card.name}
+                gender={card.gender}
+                email={card.email}
+              />
             ))}
+          </div>
+        ) : (
+          /** Condional: if the Api doesnot work or fetch the data  */
+          <div className="flex justify-center h-full bg-red-400	text-white font-bold	">
+            <span>Data not fetched from API.</span>
           </div>
         )}
       </main>
 
-      <footer className="bg-gray-800 text-white py-4 px-8">
+      <footer className=" flex justify-center bg-gray-800 text-white py-4 px-8">
         {/* Footer content */}
         <p>&copy; React First Application</p>
       </footer>
--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-interface CardProps {
+export interface CardProps {
   id: { value: string };
   name: { first: string; last: string; title: string };
   gender: string;
@@ -11,12 +11,12 @@ const Card: React.FC<CardProps> = ({ id, name, gender, email }) => {
   return (
     <div
       key={id.value}
-      className="bg-gray-200 rounded-lg shadow-md p-6 border border-4 border-indigo-200 border-y-indigo-500"
+      className=" bg-gray-200 flex flex-col justify-center items-center mt-8 rounded-lg shadow-md p-6 border-4 border-indigo-200 border-y-indigo-500"
     >
-      <h2 className="text-2xl font-bold mb-4">
+      <h2 className="text-2xl font-bold mb-4 ">
         {name.title} {name.first} {name.last}
       </h2>
-      <p className="text-black-200">Gender: {gender}</p>
+      <p className="text-black-200 ">Gender: {gender}</p>
       <p className="text-black-200">Email: {email}</p>
     </div>
   );
